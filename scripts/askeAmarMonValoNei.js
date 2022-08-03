82e09f42cc5f51e7c07a087ba41db01fcb31d066
maruf <lancerboy284@gmail.com>
2022-08-03 16:00:15 +0600

    aske amar mon valo nei

---
+++ scripts/askeAmarMonValoNei.js
@@ -0,0 +1,21 @@
+function clickMe() {
+  for (let i = 1; i <= 39; i++) {
+    // console.log("aske amar mon valo nei");
+  }
+}
+// clickMe();
+
+// adding addEventListener
+let getBtn = document.getElementById("click-btn");
+getBtn.addEventListener("click", function () {
+  // clickMe();
+  // let showTxt = getBtn.innerText;
+  for (let i = 1; i <= 9999; i++) {
+    // showTxt = "aske amar mon valo nei";
+    // console.log(showTxt);
+    let createBtn = document.createElement("button");
+    createBtn.innerHTML = "aske amar mon valo nei";
+    getBtn.innerText = createBtn.innerHTML;
+    document.write(createBtn.innerHTML + "<br/>");
+  }
+});
